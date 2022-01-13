2372d04c0c6dda4d5d4c0d5836b146914ce39ec2
RuthTadesse <tib6297@gmail.com>
2022-01-13 17:48:15 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+
+let c = 2000;
+let d = 100;
+function divide(){
+ return c / d
+}
+
+
+
+let x = 2;
+const square = function(multiply){
+return x * x
+}
+
+
+
+
+    let a = 3;
+    let b = 4;
+const add = function(){
+return a + b;
+}
